4339db5ff6d51fb6cf1319294b0a14ed645c6e46
Vanessa Sibley <sibley.vanessas@gmail.com>
2022-03-07 08:21:36 -0800

    moved cards into seperate component & added styles

--- src/componenets/card-list/card-list.components.jsx
+++ src/componenets/card-list/card-list.components.jsx
@@ -1,19 +1,18 @@
 import { Component } from 'react';
+import Card from '../card/card.component'
+import './card-list.styles.css'
 
 class CardList extends Component {
     render() {
-        console.log('render from cardlist')
         const { monsters } = this.props;
 
         return (
-            <div>
-                {monsters.map(monster => (
-                    <div className='card-container' key={monster.id}>
-                        <img alt={'monster {monster.name}'} src={`https://robohash.org/${monster.id}1?set=set2`} />
-                        <h2>{monster.name}</h2>
-                        <p>{monster.email}</p>
-                    </div>
-                ))}
+            <div className='card-list'>
+                {monsters.map((monster) => {
+                    return (
+                        <Card monster={monster} />
+                    );
+                })}
             </div>
         );
     }
